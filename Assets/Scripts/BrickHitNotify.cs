a7489fd9911a31e817104386ea9fd14a1c1938f4
Daniel C <63077752+dcrep@users.noreply.github.com>
2026-02-04 22:28:31 -0500

    Brick-other collision + sound

--- Assets/Scripts/BrickHitNotify.cs
+++ Assets/Scripts/BrickHitNotify.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BrickHitNotify : MonoBehaviour
 {
     // event
-    public delegate void BrickHitEventHandler(GameObject brick, GameObject collider);
+    public delegate void BrickHitEventHandler(GameObject brick, GameObject collider, Vector3 hitPoint);
     public static event BrickHitEventHandler OnBrickHit;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,6 +20,6 @@ public class BrickHitNotify : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        OnBrickHit?.Invoke(gameObject, collision.gameObject);
+        OnBrickHit?.Invoke(gameObject, collision.gameObject, collision.contacts[0].point);
     }
 }
